9979bf7b920a93d4f34207c372e43b28943667ed
Иван К. PC <itehnoviking@gmail.com>
2022-01-04 01:08:11 +0300

    Add Dependency injection, CarsController + View (Index)

---
+++ AspNetCoreShopTest/Controllers/CarsController.cs
@@ -0,0 +1,23 @@
+﻿using AspNetCoreShopTest.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCoreShopTest.Controllers
+{
+    public class CarsController : Controller
+    {
+        private readonly IAllCars _allCars;
+        private readonly ICarsCategory _allCarsCategory;
+
+        public CarsController(IAllCars iAllCars, ICarsCategory iCarsCat)
+        {
+            _allCars = iAllCars;
+            _allCarsCategory = iCarsCat;
+        }
+
+        public IActionResult Index()
+        {
+            var cars = _allCars.Cars;
+            return View(cars);
+        }
+    }
+}
